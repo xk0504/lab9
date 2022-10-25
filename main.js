e7f21e9c92e0313e6168b720e20c6aee328c45ef
xk0504 <111686992+xk0504@users.noreply.github.com>
2022-10-25 20:17:43 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//function that returns the sum of squares of two given numbers.
+let sumsquare = function (a, b) {
+result = ((a * a) + (b * b))
+return result
+}
+
+let a = 3;
+let b = 4;
+let c = sumsquare (a, b); 
+console.log (c)
+
+//Function that return product of three given numbers
+let sumofthree = function (l, m, n ) {
+	result = (l+ m+ n)
+	return result
+}
+let l = 10;
+let m = 15;
+let n = 50;
+let k = sumofthree (l, m, n);
+console.log (k)
+
+//function that returns the biggest among two numbers
+
+let x = 10;
+let y = 15;
+if( x > y ) {
+console.log (x)
+}
+else if (x == y ) {
+console.log ("x and y are equal")
+}
+else {
+console.log(y)
+}
